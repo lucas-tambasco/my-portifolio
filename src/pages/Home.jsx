f2c20a2b2989628083917591b3a2b713e19de82a
Lucas Tambasco <lucasgtambasco@gmail.com>
2025-09-15 09:39:18 -0300

    feat: add stars background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,19 @@
+import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
 export const Home = () => {
-    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+      <StarBackground />
 
-        {/* Theme Toggle */}
-            <ThemeToggle />
-        {/* Background Effects */}
+      {/* NavBar */}
 
-        {/* NavBar */}
+      {/* Main Content */}
 
-        {/* Main Content */}
-
-        {/* Footer */}
-
-
-    </div>;
-}
+      {/* Footer */}
+    </div>
+  );
+};
